503ea84a2f7b2ddaaacaecd97968917693aba905
Adam Wolfe Gordon <awg@digitalocean.com>
2019-09-26 16:22:02 -0600

    give create-cluster-action some outputs

--- .github/actions/create-cluster-action/entrypoint.sh
+++ .github/actions/create-cluster-action/entrypoint.sh
@@ -8,7 +8,10 @@ RANDOM_STRING=$(hexdump -n 12 -e '3/4 "%08X" 1 "\n"' /dev/random)
 NAME=test-cluster-$(echo ${RANDOM_STRING} | tr '[:upper:]' '[:lower:]')
 
 echo "Creating k8s cluster: ${NAME}"
-/app/doctl kubernetes cluster create ${NAME}
-/app/doctl kubernetes cluster kubeconfig save ${NAME}
+/app/doctl kubernetes cluster create ${NAME} --update-kubeconfig=false
+CLUSTER_ID=$(/app/doctl kubernetes cluster get ${NAME} | grep ${NAME} | awk '{print $1}')
+KUBECONFIG=$(/app/doctl kubernetes cluster kubeconfig show ${NAME} | base64)
 
-ls -al /github/home/.kube
+echo "::set-output name=CLUSTER_NAME::${NAME}"
+echo "::set-output name=CLUSTER_ID::${CLUSTER_ID}"
+echo "::set-output name=KUBECONFIG::${KUBECONFIG}"
